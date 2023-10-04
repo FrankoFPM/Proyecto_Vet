fb295955c6721b277c0fc687588e314cefb5f2cd
Franco <frank.13.gp@gmail.com>
2023-10-04 07:09:28 -0500

    DB

---
+++ src/DB/DB_Vet.sql
@@ -0,0 +1,92 @@
+drop database if exists vet;
+create database vet;
+use vet;
+
+create table cargo(
+	id_cargo int primary key auto_increment not null,
+    descripcion varchar(15)
+);
+
+create table personal(
+	id_personal int primary key auto_increment not null,
+    nombre varchar(15) not null,
+    -- apellido
+    -- dni
+    pass varchar(10) not null,
+    cargo int,
+    FOREIGN KEY (cargo) REFERENCES cargo(id_cargo)
+);
+
+create table cliente(
+	id_cliente varchar(8) primary key not null,
+    nombre varchar(15) not null,
+    apellido varchar(15) not null,
+    telefono varchar(12) not null,
+    correo varchar(50) not null,
+    dni int not null,
+    direccion varchar(100) not null
+);
+insert into cliente(id_cliente, nombre, apellido, telefono, correo, dni, direccion)
+VALUES ('CLI-0001', 'Juan', 'Pérez', '123-456-7890', 'juan@example.com', 123456789, '123 Calle Principal');
+
+DELIMITER //
+CREATE PROCEDURE ValidarCredenciales(
+    IN p_nombre_usuario VARCHAR(15),
+    IN p_contraseña VARCHAR(15),
+    OUT valido BOOLEAN
+)
+BEGIN
+    DECLARE v_cont INT;
+    SELECT COUNT(*) INTO v_cont
+    FROM personal
+    WHERE nombre = p_nombre_usuario AND pass = p_contraseña;
+    
+    IF v_cont = 1 THEN
+        SET valido = TRUE;
+    ELSE
+        SET valido = FALSE;
+    END IF;
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_codigoCliente(OUT codigo_generado CHAR(8))
+BEGIN
+    DECLARE serie CHAR(4);
+    DECLARE max_codigo_existente INT;
+
+    SELECT MAX(CAST(SUBSTRING(id_cliente, 5) AS SIGNED)) INTO max_codigo_existente FROM cliente;
+
+    IF max_codigo_existente IS NULL THEN
+        SET codigo_generado = 'CLI-0001';
+    ELSE
+        SET serie = 'CLI-';
+        SET codigo_generado = CONCAT(serie, LPAD(max_codigo_existente + 1, 4, '0'));
+    END IF;
+END //
+DELIMITER ;
+
+CALL sp_codigoCliente(@xcodigoCliente);
+SELECT @xcodigoCliente;
+
+SELECT * FROM personal WHERE nombre = '<username>' AND pass = '' OR '1'='1';
+
+
+CALL ValidarCredenciales('Juan', "123", @resultado);
+SELECT @resultado;
+
+
+-- Insertar datos en la tabla "cargo"
+INSERT INTO cargo (descripcion) VALUES
+    ('Gerente'),
+    ('Supervisor'),
+    ('Asistente'),
+    ('Técnico');
+
+-- Insertar datos en la tabla "personal"
+INSERT INTO personal (nombre, pass, cargo) VALUES
+    ('Juan', '123', 1),
+    ('Maria', '123', 2),
+    ('Carlos', '123', 3),
+    ('Luis', '123', 4);  
