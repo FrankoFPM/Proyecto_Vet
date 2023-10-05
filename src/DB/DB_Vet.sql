43bde8de952f565ef28616b3c525ff650cb72d23
Franco <frank.13.gp@gmail.com>
2023-10-04 20:48:02 -0500

    update DB

--- src/DB/DB_Vet.sql
+++ src/DB/DB_Vet.sql
@@ -49,7 +49,7 @@ BEGIN
 END;
 //
 DELIMITER ;
-
+/*
 DELIMITER //
 CREATE PROCEDURE sp_codigoCliente(OUT codigo_generado CHAR(8))
 BEGIN
@@ -68,6 +68,46 @@ END //
 DELIMITER ;
 
 CALL sp_codigoCliente(@xcodigoCliente);
+SELECT @xcodigoCliente;
+*/
+
+DELIMITER //
+
+CREATE PROCEDURE sp_listarDatos(IN tabla VARCHAR(20))
+BEGIN
+    SET @sql = CONCAT('SELECT * FROM ', tabla);
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END //
+DELIMITER ;
+call sp_listarDatos("cliente");
+DELIMITER //
+CREATE PROCEDURE sp_codigo_autogenerado(
+    IN tabla CHAR(64),
+    IN columna CHAR(64),
+    IN prefijo CHAR(4),
+    IN longitud_codigo INT,
+    OUT codigo_generado CHAR(64)
+)
+BEGIN
+    DECLARE max_codigo_existente INT;
+    DECLARE consulta VARCHAR(1000);
+
+    SET @consulta = CONCAT('SELECT MAX(CAST(SUBSTRING(', columna, ', ', LENGTH(prefijo) + 1, ') AS SIGNED)) INTO @max_codigo_existente FROM ', tabla);
+    PREPARE stmt FROM @consulta;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    IF @max_codigo_existente IS NULL THEN
+        SET codigo_generado = CONCAT(prefijo, LPAD('1', longitud_codigo, '0'));
+    ELSE
+        SET codigo_generado = CONCAT(prefijo, LPAD(@max_codigo_existente + 1, longitud_codigo, '0'));
+    END IF;
+END //
+DELIMITER ;
+
+CALL sp_codigo_autogenerado("cliente","id_cliente","CLI-",4,@xcodigoCliente);
 SELECT @xcodigoCliente;
 
 SELECT * FROM personal WHERE nombre = '<username>' AND pass = '' OR '1'='1';
