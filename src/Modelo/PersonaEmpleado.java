e22582a001d5e34f7f85aed19c33f3a4c1dec1f7
Franco <frank.13.gp@gmail.com>
2023-10-11 01:15:39 -0500

    Modify superClass

--- src/Modelo/PersonaEmpleado.java
+++ src/Modelo/PersonaEmpleado.java
@@ -1,5 +1,43 @@
 package Modelo;
 
-public class PersonaEmpleado {
+public class PersonaEmpleado extends Persona {
+
+    private String contraseña;
+    private String cargo;
+    private String nickname;
+
+    public PersonaEmpleado() {
+    }
     
+    public PersonaEmpleado(String contraseña, String cargo, String nickname, String codigo, String nombre, String apellido, String correo, int dni) {
+        super(codigo, nombre, apellido, correo, dni);
+        this.contraseña = contraseña;
+        this.cargo = cargo;
+        this.nickname = nickname;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
 }
