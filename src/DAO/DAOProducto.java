c43a266ff36e610b94085c6551a7b8fbb9bc56b2
Franco <frank.13.gp@gmail.com>
2023-11-20 01:17:11 -0500

    Refactor UI_InventarioController and add DAOProducto

---
+++ src/DAO/DAOProducto.java
@@ -0,0 +1,74 @@
+package DAO;
+
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import Modelo.ProductoInventario;
+import Procesos.ProcesoListado;
+import Procesos.ProcesoRD;
+
+public class DAOProducto extends ConexionDB {
+    public void insertarProducto(ProductoInventario producto) {
+        CallableStatement cs_insert;
+        try {
+            cs_insert = conectar.prepareCall("{CALL sp_insertar_producto(?,?,?,?,?,?,?)}");
+            cs_insert.setString(1, producto.getCodigo());
+            cs_insert.setString(2, producto.getNombre());
+            cs_insert.setString(3, producto.getMarca());
+            cs_insert.setDouble(4, producto.getPrecio());
+            cs_insert.setInt(5, producto.getCantidad());
+            cs_insert.setString(6, producto.getCategoria());
+            cs_insert.setDate(7, producto.getFecha());
+            int resultado = cs_insert.executeUpdate();
+            cs_insert.close();
+            if (resultado > 0) {
+                JOptionPane.showMessageDialog(null, "Producto registrado", "Success", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al registrar (˘･_･˘)", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizarProducto(ProductoInventario producto) {
+        CallableStatement cs_update;
+        try {
+            cs_update = conectar.prepareCall("{CALL sp_actualizar_producto(?,?,?,?,?,?,?)}");
+            cs_update.setString(1, producto.getCodigo());
+            cs_update.setString(2, producto.getNombre());
+            cs_update.setString(3, producto.getMarca());
+            cs_update.setDouble(4, producto.getPrecio());
+            cs_update.setInt(5, producto.getCantidad());
+            cs_update.setString(6, producto.getCategoria());
+            cs_update.setDate(7, producto.getFecha());
+            int resultado = cs_update.executeUpdate();
+            cs_update.close();
+            if (resultado > 0) {
+                JOptionPane.showMessageDialog(null, "Producto modificado", "Success", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al modificar (˘･_･˘)", "Error", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // metodo eliminar producto
+    public void eliminarProducto(String codigo) {
+        ProcesoRD.eliminarRegistros("Productos", "id_producto", codigo);
+    }
+
+    // metodo buscar producto retorna un List<String[]>
+    public List<String[]> buscarProducto(String codigo) {
+        return ProcesoRD.buscarRegistros("Productos", "id_producto", codigo);
+    }
+
+    // metodo listar productos retorna un List<String[]>
+    public List<String[]> listarProductos() {
+        return ProcesoListado.listarDatos("productos");
+    }
+}
