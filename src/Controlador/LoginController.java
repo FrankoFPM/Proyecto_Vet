63516b4bb2d9fd5c4de5f429eabc4af1c9889817
Franco <frank.13.gp@gmail.com>
2023-11-21 17:19:28 -0500

    Refactor login authentication method

--- src/Controlador/LoginController.java
+++ src/Controlador/LoginController.java
@@ -1,17 +1,12 @@
 package Controlador;
 
-import java.sql.Connection;
+import static DAO.MetodosReadDelete.Autenticar;
 
-import java.sql.SQLException;
-import java.sql.CallableStatement;
-import java.sql.Types;
 import Vista.Login;
 import Vista.Dashboard_UI;
-import DB.Conexion;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
 
 public class LoginController implements ActionListener {
 
@@ -33,40 +28,7 @@ public class LoginController implements ActionListener {
         vLogin.setVisible(true);
     }
 
-    public static void Autenticar(Login lg, Dashboard_UI dash) {
-        Conexion objConn = new Conexion();
-        Connection cn = objConn.ObtenerConexion();
-        CallableStatement cs_Login;
-
-        String user = lg.txtUser.getText();
-        char[] passChars = lg.txtPass.getPassword();
-        String pass = new String(passChars);
-
-        try {
-            String query = "{call ValidarCredenciales(?,?,?)}";
-            cs_Login = cn.prepareCall(query);
-            cs_Login.setString(1, user);
-            cs_Login.setString(2, pass);
-            cs_Login.registerOutParameter(3, Types.BOOLEAN);
-            cs_Login.execute();
-            boolean resultado = cs_Login.getBoolean(3);
-            cs_Login.close();
-            if (resultado) {
-                JOptionPane.showMessageDialog(lg, "Inicio de sesión exitoso", "Success", JOptionPane.INFORMATION_MESSAGE);
-                lg.dispose();
-                //dash.labelUsername.setText("Bienvenido, " + user.toUpperCase());
-                DashboardController controllerDash = new DashboardController(dash, user.toUpperCase());
-                /*dash.getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.WHITE);
-                dash.getRootPane().putClientProperty("JRootPane.titleBarBackground", new Color(23, 180, 252));
-                dash.setVisible(true);*/
-            } else {
-                JOptionPane.showMessageDialog(lg, "Credenciales incorrectas", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-            objConn.CerrarConexion();
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error de base de datos: " + e.getMessage());
-        }
-    }
+    
 
     @Override
     public void actionPerformed(ActionEvent e) {
--- src/DAO/MetodosReadDelete.java
+++ src/DAO/MetodosReadDelete.java
@@ -1,12 +1,16 @@
 package DAO;
 
+import Controlador.DashboardController;
 import DB.Conexion;
+import Vista.Dashboard_UI;
+import Vista.Login;
 import java.awt.HeadlessException;
 import java.util.List;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -111,5 +115,40 @@ public class MetodosReadDelete {
         } catch (HeadlessException | SQLException e) {
         }
         return filas;
+    }
+    
+    public static void Autenticar(Login lg, Dashboard_UI dash) {
+        Conexion objConn = new Conexion();
+        Connection cn = objConn.ObtenerConexion();
+        CallableStatement cs_Login;
+
+        String user = lg.txtUser.getText();
+        char[] passChars = lg.txtPass.getPassword();
+        String pass = new String(passChars);
+
+        try {
+            String query = "{call ValidarCredenciales(?,?,?)}";
+            cs_Login = cn.prepareCall(query);
+            cs_Login.setString(1, user);
+            cs_Login.setString(2, pass);
+            cs_Login.registerOutParameter(3, Types.BOOLEAN);
+            cs_Login.execute();
+            boolean resultado = cs_Login.getBoolean(3);
+            cs_Login.close();
+            if (resultado) {
+                JOptionPane.showMessageDialog(lg, "Inicio de sesión exitoso", "Success", JOptionPane.INFORMATION_MESSAGE);
+                lg.dispose();
+                //dash.labelUsername.setText("Bienvenido, " + user.toUpperCase());
+                DashboardController controllerDash = new DashboardController(dash, user.toUpperCase());
+                /*dash.getRootPane().putClientProperty("JRootPane.titleBarForeground", Color.WHITE);
+                dash.getRootPane().putClientProperty("JRootPane.titleBarBackground", new Color(23, 180, 252));
+                dash.setVisible(true);*/
+            } else {
+                JOptionPane.showMessageDialog(lg, "Credenciales incorrectas", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            objConn.CerrarConexion();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de base de datos: " + e.getMessage());
+        }
     }
 }
